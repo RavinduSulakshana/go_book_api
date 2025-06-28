115aed1d06e8c6979064f095d33809c8f6001009
RavinduSulakshana <ravindusulakshana2525@gmail.com>
2025-06-29 00:38:09 +0530

    book api is initiate

---
+++ api/handlers.go
@@ -0,0 +1,27 @@
+package api
+
+import (
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func InitDB() {
+	err := godotenv.LOAD()
+	if err != nill {
+		log.Fatal("failed to connect database", err)
+	}
+
+	dsn := os.Getenv("DB_URL")
+	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal("failed to connect to database", err)
+	}
+	//migrate the schema
+
+}
